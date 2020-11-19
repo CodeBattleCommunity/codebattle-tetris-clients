464762497ad97abdfe731bd6b71547039a889c78
Yaroslav Zhavoronkov <yaroslav_zhavoronkov@epam.com>
2020-11-19 08:36:25 +0300

    Add hot-fix class UrlWrapper to procces URL. Fix UrlParser from codenjoy repo to handle game parameter.

--- CodeBattleJava/src/main/java/com/codenjoy/dojo/tetris/client/YourSolver.java
+++ CodeBattleJava/src/main/java/com/codenjoy/dojo/tetris/client/YourSolver.java
@@ -10,12 +10,12 @@ package com.codenjoy.dojo.tetris.client;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -27,7 +27,6 @@ import com.codenjoy.dojo.client.AbstractJsonSolver;
 import com.codenjoy.dojo.client.WebSocketRunner;
 import com.codenjoy.dojo.services.Dice;
 import com.codenjoy.dojo.services.RandomDice;
-import org.json.JSONObject;
 
 /**
  * User: your name
@@ -51,7 +50,7 @@ public class YourSolver extends AbstractJsonSolver<Board> {
     public static void main(String[] args) {
         WebSocketRunner.runClient(
                 // paste here board page url from browser after registration
-                "http://localhost:8080/codenjoy-contest/board/player/ziwpjz46y4z5567k7uup?code=3867579515136108220&gameName=tetris",
+                new UrlWrapper().removeTrailingParameters("http://localhost:8080/codenjoy-contest/board/player/ziwpjz46y4z5567k7uup?code=3867579515136108220&gameName=tetris"),
                 new YourSolver(new RandomDice()),
                 new Board());
     }
