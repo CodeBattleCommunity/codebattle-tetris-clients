084f304a99e65fb581c627d75c7fbd59cadfaad3
Phoenix100101 <dnsk92@gmail.com>
2020-11-29 17:57:45 +0400

    include command builder, delete redundant files

--- CodeBattleCPP/CodeBattleCpp/CodeBattleCpp.cpp
+++ CodeBattleCPP/CodeBattleCpp/CodeBattleCpp.cpp
@@ -2,18 +2,25 @@
 #include <random>
 
 #include "GameClientTetris.h"
+#include "CommandBuilder.h"
 
 void main()
 {
 	srand(time(0));
 
-	GameClientTetris *gcb = new GameClientTetris("http://localhost:8080/codenjoy-contest/board/player/9sahhltizo730tp22kr1?code=7816623675811590848&gameName=tetris");
+	GameClientTetris *gcb = new GameClientTetris("http://epruryaw0537.moscow.epam.com/codenjoy-contest/board/player/9sahhltizo730tp22kr1?code=7816623675811590848&gameName=tetris");
 	gcb->Run([&]()
 	{
-			//ÏÈÑÀÒÜ ÊÎÄ ÇÄÅÑÜ!!!!!
+			//write down code here
 			GameBoard* gb = gcb->get_GameBoard();
 			GlassBoard* glassBoard = gb->getGlassBoard();
-			gcb->LoderunnerAction("LEFT");
+			CommandBuilder builder;
+			builder.addCommand(Command::LEFT);
+			builder.addCommand(Command::LEFT);
+			builder.addCommand(Command::DOWN);
+			std::string action = builder.buildString();
+
+			gcb->sendAction(action);
 	});
 
 	getchar();
